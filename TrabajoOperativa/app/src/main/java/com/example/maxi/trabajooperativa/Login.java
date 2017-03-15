4cdfaa84fda83773b62ad0419d00dc67114f3c2d
Maxi <maxiroselli@gmail.com>
2017-03-15 20:06:43 -0300

    Inicio
    Trabajo inicial y ejemplo de como llamar de una interfaz a la otra

---
+++ TrabajoOperativa/app/src/main/java/com/example/maxi/trabajooperativa/Login.java
@@ -0,0 +1,11 @@
+package com.example.maxi.trabajooperativa;
+
+import android.app.Activity;
+
+/**
+ * Created by Maxi on 14/3/2017.
+ */
+
+public class Login extends Activity {
+
+}
---
+++ TrazaAppRec/app/src/main/java/com/trazaapprec/main/SegundaActividad.java
@@ -0,0 +1,14 @@
+package com.trazaapprec.main;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SegundaActividad extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_actividad);
+    }
+
+}
