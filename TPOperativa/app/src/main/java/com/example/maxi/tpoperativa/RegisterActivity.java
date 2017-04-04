604e0acec8ca1cd4533bdf5508619b7214df3a66
Maxi <maxiroselli@gmail.com>
2017-04-03 21:40:32 -0300

    Problemas en Insert
    Falta ver que metodo hay que ejecutar en el statement del insert para
    que agregue los datos a la base. Esta ahi el tema, no sale de eso. EN
    MySQL hice un metodo executeQuery que tendria que agregar los datos, sin
    retornar un ResultSet. Si anda eso, ya insertamos en la base sin
    problemas

--- TPOperativa/app/src/main/java/com/example/maxi/tpoperativa/RegisterActivity.java
+++ TPOperativa/app/src/main/java/com/example/maxi/tpoperativa/RegisterActivity.java
@@ -9,7 +9,9 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -31,6 +33,8 @@ public class RegisterActivity extends AppCompatActivity {
     private TextView Telefono;
     private TextView Password;
     private Spinner spinnerCities;
+    private Button btnFinalizar;
+    private int itemSpinner;
 
     private List listCities = new ArrayList<>();
 
@@ -39,6 +43,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("Crear Cuenta");
         setContentView(R.layout.activity_register);
+        this.itemSpinner = 0;
 
         this.Nombre = (TextView) findViewById(R.id.txt_nombre);
         this.Nombre.setOnClickListener(new View.OnClickListener() {
@@ -81,12 +86,46 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        this.Password = (TextView) findViewById(R.id.txt_password);
+        this.Password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(String.valueOf(Password.getText()).equals("Email"))
+                    Password.setText("");
+            }
+        });
+
         this.spinnerCities = (Spinner) findViewById(R.id.spinner);
         updateSpinner();
 
-        if(verificaDatos()){
-            //Llamar a insert a la base calculando la posicion donde esta el tipo
-        }
+        this.spinnerCities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> parentView,View selectedItemView, int position, long id) {
+                itemSpinner = spinnerCities.getSelectedItemPosition() + 1;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                itemSpinner = 0;
+            };
+        });
+
+        this.btnFinalizar = (Button) findViewById(R.id.btnFinalizar);
+        this.btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(verifyDate()){
+                    AddUserTask add = new AddUserTask(Usuario.getText(),Password.getText().toString(),
+                    Nombre.getText(),
+                    Email.getText(),
+                    Direccion.getText(),
+                    Telefono.getText());
+                    add.execute();
+                    //Llamar a insert a la base calculando la posicion donde esta el tipo
+                }
+            }
+        });
+
+
 
     }
 
@@ -96,10 +135,11 @@ public class RegisterActivity extends AppCompatActivity {
         citiesTask.execute();
 
     }
-    private boolean verificaDatos() {
-        if(String.valueOf(this.Nombre.getText()).length() != 0 || String.valueOf(this.Email.getText()).length() != 0
-                ||String.valueOf(this.Usuario.getText()).length() != 0 || String.valueOf(this.Direccion.getText()).length() != 0
-                ||String.valueOf(this.Telefono.getText()).length() != 0 || String.valueOf(this.Password.getText()).length() != 0  )
+    private boolean verifyDate() {
+        if(String.valueOf(this.Nombre.getText()).length() != 0 && String.valueOf(this.Email.getText()).length() != 0
+                && String.valueOf(this.Usuario.getText()).length() != 0 && String.valueOf(this.Direccion.getText()).length() != 0
+                &&String.valueOf(this.Telefono.getText()).length() != 0 && String.valueOf(this.Password.getText()).length() != 0
+                && (itemSpinner != 0) )
             return true;
         return false;
     }
@@ -140,5 +180,62 @@ public class RegisterActivity extends AppCompatActivity {
             spinnerCities.setAdapter(spinner_adapter);
         }
 
+    }
+
+    public class AddUserTask extends AsyncTask<Void, Void, Boolean>{
+        private String Usuario;
+        private String Password;
+        private String Nombre;
+        private String Email;
+        private String Direccion;
+        private String Telefono;
+
+
+        public AddUserTask(CharSequence Usuario, String Password, CharSequence Nombre, CharSequence Email,
+                           CharSequence Direccion, CharSequence Telefono){
+
+            this.Usuario = String.valueOf(Usuario);
+            this.Password = Password;
+            this.Nombre = String.valueOf(Nombre);
+            this.Email = String.valueOf(Email);
+            this.Direccion =String.valueOf(Direccion);
+            this.Telefono = String.valueOf(Telefono);
+        };
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                String query = "SELECT * FROM USERS " +
+                               "WHERE email='"+Email+"'";
+                ResultSet resultSet = LoginActivity.Sql.getResultset(query);
+
+                if(resultSet.next()) {
+                    return false;
+                }
+                query = "INSERT INTO USERS(id,role_id,username,password,name,email,address,phone,location_id) " +
+                        "VALUES(`id`,1," +
+                        this.Usuario+","+
+                        this.Password+"," +
+                        this.Nombre+"," +
+                        this.Email+"," +
+                        this.Direccion+"," +
+                        this.Telefono+"," +
+                        String.valueOf(itemSpinner)+")";
+                LoginActivity.Sql.executeQuery(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return true;
+        }
+        protected void onPostExecute(final Boolean sucess) {
+          if (sucess) {
+              //GENERAR LAS COORDENADAS
+
+              finish();
+            } else {
+
+            }
+
+        }
     }
 }
